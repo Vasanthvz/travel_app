fff472189e445ddbd8849db72808e43936c860ee
vasanthvz <vasanthv1373@gmail.com>
2023-04-04 15:46:54 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/Components/Navbar/Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 import './Navbar.css'
+import {MdOutlineTravelExplore} from 'react-icons/md'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <section className='navBarSection'>
+        <header className="header flex">
+            <div className="logoDiv">
+                <a href="#" className="logo flex">
+                    <h1> <MdOutlineTravelExplore className='icon'/>travel</h1>
+                </a>
+            </div>
+            <div className="navBar">
+                <ul className="navLists flex">
+                    <li className="navItems">
+                        <a href="#" className="navLinks">Home</a>
+                    </li>
+                    <li className="navItems">
+                        <a href="#" className="navLinks">Packages</a>
+                    </li>
+                    <li className="navItems">
+                        <a href="#" className="navLinks">Shop</a>
+                    </li>
+                    <li className="navItems">
+                        <a href="#" className="navLinks">About</a>
+                    </li>
+                    <li className="navItems">
+                        <a href="#" className="navLinks">Pages</a>
+                    </li>
+                    <li className="navItems">
+                        <a href="#" className="navLinks">News</a>
+                    </li>
+                    <li className="navItems">
+                        <a href="#" className="navLinks">Contact</a>
+                    </li>
+                    <button className='btn'>
+                        <a href="#">BOOK NOW</a>
+                    </button>
+                </ul>
+            </div>
+        </header>
+    </section>
   )
 }
 
