7726a4afa6cc19e09441ffe0e1b37f52823c0743
vasanthvz <vasanthv1373@gmail.com>
2023-04-04 21:07:05 +0530

    modified:   src/Components/Navbar/Navbar.css 	modified:   src/Components/Navbar/Navbar.jsx 	modified:   src/Components/Navbar/Navbar.min.css 	modified:   src/Components/Navbar/Navbar.scss

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import {TbGridDots} from 'react-icons/tb'
 
 const Navbar = () => {
+    const [active, setActive] = useState('navBar')
+    // Function to toggle navBar
+    const showNav = ()=>{
+        setActive('navBar activeNavbar')
+    }
   return (
     <section className='navBarSection'>
         <header className="header flex">
@@ -13,7 +18,7 @@ const Navbar = () => {
                     <h1> <MdOutlineTravelExplore className='icon'/>Travel</h1>
                 </a>
             </div>
-            <div className="navBar">
+            <div className={active}>
                 <ul className="navLists flex">
                     <li className="navItems">
                         <a href="#" className="navLinks">Home</a>
